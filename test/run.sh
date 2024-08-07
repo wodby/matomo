31d3d790fe12b6e39c0307379b4aba56c0cf2688
Chingis Sandanov <chingis@chingis.io>
2024-08-07 10:43:01 +0300

    Migrate docker compose to new format

--- test/run.sh
+++ test/run.sh
@@ -6,8 +6,8 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-docker-compose up -d
-docker-compose exec -T nginx make check-ready -f /usr/local/bin/actions.mk
-docker-compose exec -T matomo make init -f /usr/local/bin/actions.mk
-docker-compose exec -T matomo tests.sh
-docker-compose down
+docker compose up -d
+docker compose exec -T nginx make check-ready -f /usr/local/bin/actions.mk
+docker compose exec -T matomo make init -f /usr/local/bin/actions.mk
+docker compose exec -T matomo tests.sh
+docker compose down
